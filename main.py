c77f6caf5d5cc33716ce31d2afdfd49b48979b23
kizkiz <yetislayer7@hotmail.com>
2024-09-21 07:13:50 +1000

    Removed command line prompt

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ while True:
 
     thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
-    cv2.imshow("My video", dil_frame)
 
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -55,8 +54,6 @@ while True:
     if status_list[0] == 1 and status_list[1] == 0:
         email_thread = Thread(target=send_email, args=(image_with_object, ))
         email_thread.daemon = True
-        clean_thread = Thread(target=clean_folder)
-        clean_thread.daemon = True
 
         email_thread.start()
 
@@ -68,4 +65,4 @@ while True:
 
 video.release()
 
-clean_thread.start()
+clean_folder()
